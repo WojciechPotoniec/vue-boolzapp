bbf72e2e65cb1c22965b81e665bc38f34e339fb2
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-04 13:43:13 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
 import { contacts } from "./data.js";
 
-const {createApp} = Vue;
+const dt = luxon.DateTime;
+const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            contacts,
-            activeContactId: 1
-        }
+  data() {
+    return {
+      contacts,
+      activeContactId: 1,
+      msgText: "",
+      searchText: "",
+    };
+  },
+  methods: {
+    createMsg() {
+      const newMessage = {
+        date: dt.now().setLocale("it").toFormat("dd/MM/yyyy HH:mm:ss"),
+        message: this.msgText,
+        status: "sent",
+      };
+      return newMessage;
     },
-    methods:{},
-    computed: {
-        activeContact() {
-            return this.contacts.find((el)=> el.id === this.activeContactId);
+    addMessage() {
+      const newMessage = {
+        date: dt.now().setLocale("it").toFormat("dd/MM/yyyy HH:mm:ss"),
+        message: this.msgText,
+        status: "sent",
+      };
+      this.activeContact.messages.push(newMessage);
+      this.msgText = "";
+      setTimeout(() => {
+        const newMessage = {
+          date: dt.now().setLocale("it").toFormat("dd/MM/yyyy HH:mm:ss"),
+          message: "ok",
+          status: "received",
+        };
+        this.activeContact.messages.push(newMessage);
+      }, 1000);
     },
-},
-    mounted() {
+  },
+  computed: {
+    activeContact() {
+      return this.contacts.find((el) => el.id === this.activeContactId);
     },
-}).mount('#app')
+    filteredContacts() {
+      return this.contacts.filter((el) =>
+        el.name.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    },
+  },
+  mounted() {},
+}).mount("#app");
